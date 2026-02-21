4b75aaddba8af7d5446d80ae3c95efaf9b91e080
PrzemoLebioda <przemyslaw.lebioda@gmail.com>
2026-02-21 15:00:46 +0100

    Add files via upload

---
+++ src/main/java/com/comida/sia/StokInvestmentAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.comida.sia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StokInvestmentAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StokInvestmentAssistantApplication.class, args);
+	}
+
+}
