2015e43e919fa23d7eba48e66503c81edca5d604
PrzemoLebioda <przemyslaw.lebioda@gmail.com>
2026-02-21 15:00:11 +0100

    Delete exchangequote directory

--- exchangequote/port/acquirer/InterdayExchangeQuotesAcquirer.java
+++
@@ -1,5 +0,0 @@
-package com.comida.sia.exchangequote.port.acquirer;
-
-public interface InterdayExchangeQuotesAcquirer {
-	public ExchangeQuotesResult gatherInterdayExchangeQuoteFor(String symbol, String appliedPeriod);
-}
